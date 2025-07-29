c9da191cf1b515629b48367ba57e240ed0b22e5f
barreirakkjk <91769692+eduardo14579b@users.noreply.github.com>
2025-07-29 10:05:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ const app = express();
 const empresas = require('./rotas/empresas');
 
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('API SAE rodando!');
-});
+app.get('/', (req, res) => res.send('API SAE rodando!'));
 app.use('/empresas', empresas);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${5432}`));
