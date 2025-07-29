838e7560b3061f9e3a8dc8a7f3a0bb3b8f84cc8f
barreirakkjk <91769692+eduardo14579b@users.noreply.github.com>
2025-07-29 10:03:35 -0300

    Update empresas.js

--- empresas.js
+++ empresas.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const conexao = require('../conexao');
 
-// Rota GET /empresas
 router.get('/', async (req, res) => {
   try {
     const resultado = await conexao.query('SELECT * FROM empresas');
